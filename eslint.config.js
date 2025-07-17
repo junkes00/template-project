63431d271d8c998631f9f44a1264919984309eff
Nicolas Junkes <nicolas.junkes00@gmail.com>
2025-07-17 16:57:15 -0300

    chore(eslint): add new rules and configs on eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 import js from "@eslint/js";
 import stylistic from "@stylistic/eslint-plugin";
-import pluginReact from "eslint-plugin-react";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
@@ -9,8 +11,13 @@ export default defineConfig([
   { files: ["**/*.{js,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
 
+  js.configs.recommended,
+  tseslint.configs.recommended,
   tseslint.configs.stylistic,
-  pluginReact.configs.flat.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat["jsx-runtime"],
+  reactHooks.configs["recommended-latest"],
+  reactRefresh.configs.vite,
 
   globalIgnores(["dist", ".config/*"]),
 
@@ -22,6 +29,7 @@ export default defineConfig([
 
   {
     plugins: {
+      "react-hooks": reactHooks,
       "@stylistic": stylistic,
     },
     rules: {
@@ -32,7 +40,9 @@ export default defineConfig([
         "windows",
       ],
       "react/react-in-jsx-scope": "off",
-      // "@stylistic/indent": ["error", 2],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "@stylistic/indent": ["error", 2],
       "@stylistic/jsx-one-expression-per-line": "off",
       "@stylistic/no-multiple-empty-lines": "off",
       "@stylistic/comma-dangle": "off",
