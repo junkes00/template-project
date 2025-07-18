0d33a98d5ed7d49880a3223e18270f2de0115b00
Nicolas Junkes <nicolas.junkes00@gmail.com>
2025-07-18 16:36:07 -0300

    chore: add new rules on eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import js from "@eslint/js";
 import stylistic from "@stylistic/eslint-plugin";
+import importPlugin from "eslint-plugin-import";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 import reactPlugin from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -8,43 +10,70 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  { files: ["**/*.{js,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  globalIgnores(["dist", ".config/*"]),
 
   js.configs.recommended,
+  jsxA11y.flatConfigs.recommended,
   tseslint.configs.recommended,
-  tseslint.configs.stylistic,
   reactPlugin.configs.flat.recommended,
   reactPlugin.configs.flat["jsx-runtime"],
   reactHooks.configs["recommended-latest"],
   reactRefresh.configs.vite,
 
-  globalIgnores(["dist", ".config/*"]),
+  { files: ["**/*.{js,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
 
+  importPlugin.flatConfigs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:import/recommended",
+      // the following lines do the trick
+      "plugin:import/typescript",
+    ],
+    settings: {
+      "import/resolver": {
+        // You will also need to install and configure the TypeScript resolver
+        // See also https://github.com/import-js/eslint-import-resolver-typescript#configuration
+        typescript: true,
+        node: true,
+      },
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "import/no-dynamic-require": "warn",
+      "import/no-nodejs-modules": "warn",
+    },
+  },
+
+  tseslint.configs.stylistic,
   stylistic.configs.customize({
     indent: 2,
     quotes: "double",
     semi: true,
   }),
-
   {
-    plugins: {
-      "@stylistic": stylistic,
+    files: ["**/*.{js,ts,jsx,tsx}"], plugins: { stylistic }, rules: {
+      "@stylistic/indent": ["error", 2],
+      "@stylistic/jsx-one-expression-per-line": "error",
+      "@stylistic/no-multiple-empty-lines": "error",
+      "@stylistic/comma-dangle": "error",
     },
+  },
+  {
     rules: {
       "no-console": "warn",
-      "no-unused-vars": "warn",
-      "linebreak-style": [
-        "error",
-        "windows",
-      ],
+      "no-unused-vars": "error",
+      "linebreak-style": ["error", "windows"],
       "react/react-in-jsx-scope": "off",
       "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "@stylistic/indent": ["error", 2],
-      "@stylistic/jsx-one-expression-per-line": "off",
-      "@stylistic/no-multiple-empty-lines": "off",
-      "@stylistic/comma-dangle": "off",
+      "react-hooks/exhaustive-deps": "error",
+      "@typescript-eslint/no-unused-vars": "warn",
     },
   },
 ]);
