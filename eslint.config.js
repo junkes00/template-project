28849a611b92aedcabe2b9fa575b5886dc06440a
Nicolas Junkes <nicolas.junkes00@gmail.com>
2025-07-20 14:55:53 -0300

    chore: add husky, prettier and commitlint and new eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,79 +1,98 @@
 import js from "@eslint/js";
 import stylistic from "@stylistic/eslint-plugin";
-import importPlugin from "eslint-plugin-import";
+import prettier from "eslint-config-prettier/flat";
 import jsxA11y from "eslint-plugin-jsx-a11y";
-import reactPlugin from "eslint-plugin-react";
+import pluginReact from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  globalIgnores(["dist", ".config/*"]),
-
-  js.configs.recommended,
-  jsxA11y.flatConfigs.recommended,
-  tseslint.configs.recommended,
-  reactPlugin.configs.flat.recommended,
-  reactPlugin.configs.flat["jsx-runtime"],
+  globalIgnores(["**/*.config.*", "dist"]),
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
   reactHooks.configs["recommended-latest"],
   reactRefresh.configs.vite,
+  jsxA11y.flatConfigs.recommended,
 
-  { files: ["**/*.{js,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-
-  importPlugin.flatConfigs.recommended,
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    extends: [
-      "eslint:recommended",
-      "plugin:import/recommended",
-      // the following lines do the trick
-      "plugin:import/typescript",
-    ],
-    settings: {
-      "import/resolver": {
-        // You will also need to install and configure the TypeScript resolver
-        // See also https://github.com/import-js/eslint-import-resolver-typescript#configuration
-        typescript: true,
-        node: true,
-      },
-    },
-    rules: {
-      "no-unused-vars": "off",
-      "import/no-dynamic-require": "warn",
-      "import/no-nodejs-modules": "warn",
-    },
-  },
-
-  tseslint.configs.stylistic,
   stylistic.configs.customize({
     indent: 2,
     quotes: "double",
     semi: true,
+    arrowParens: "always",
   }),
+
   {
-    files: ["**/*.{js,ts,jsx,tsx}"], plugins: { stylistic }, rules: {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      js,
+      reactHooks,
+      jsxA11y,
+      "simple-import-sort": simpleImportSort,
+      stylistic,
+    },
+    extends: ["js/recommended"],
+    rules: {
+      "no-console": "warn",
+      "linebreak-style": ["error", "windows"],
+      "react/button-has-type": "error",
+      "no-shadow": "error",
+      "react/jsx-no-bind": "warn",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "error",
+      "react-refresh/only-export-components": "warn",
+      "@typescript-eslint/no-unused-vars": "error",
       "@stylistic/indent": ["error", 2],
       "@stylistic/jsx-one-expression-per-line": "error",
       "@stylistic/no-multiple-empty-lines": "error",
       "@stylistic/comma-dangle": "error",
+      "@stylistic/jsx-sort-props": [
+        "error",
+        {
+          ignoreCase: true,
+          shorthandFirst: true,
+          multiline: "last",
+          reservedFirst: ["key"],
+        },
+      ],
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "interface",
+          format: ["PascalCase"],
+          custom: {
+            regex: "^I[A-Z]",
+            match: true,
+          },
+        },
+      ],
+      "simple-import-sort/imports": "warn",
+      "simple-import-sort/exports": "warn",
     },
   },
+
   {
+    files: ["src/ui/components/ui/**/*.{ts,tsx}"],
     rules: {
-      "no-console": "warn",
-      "no-unused-vars": "error",
-      "linebreak-style": ["error", "windows"],
-      "react/react-in-jsx-scope": "off",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "error",
-      "@typescript-eslint/no-unused-vars": "warn",
+      "@stylistic/jsx-sort-props": "off",
+      "simple-import-sort/imports": "off",
+      "simple-import-sort/exports": "off",
+      "react-refresh/only-export-components": "off",
     },
   },
+
+  // always at the last
+  prettier,
 ]);
